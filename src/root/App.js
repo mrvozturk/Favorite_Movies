7387dcb190edf2c97f6aabdd65d84e04a2ce57a0
Merve Ozturk <ozturkmerve745@gmail.com>
2023-12-08 13:03:28 +0300

    favIconlar düzenlendi

--- src/root/App.js
+++ src/root/App.js
@@ -6,8 +6,8 @@ import favIcon from "../images/fav.png";
 function App() {
   return (
     <div className="col" style={{ display: "flex" }}>
-      <div className="movies-section" style={{ marginRight: "20px", flex: 1 }}>
-        <h3 style={{ marginLeft: 65, margin: "0 auto" }}>Movies</h3>
+      <div className="movies-section" style={{ flex: 1 }}>
+        <h3 style={{ justifyContent: "center", display: "flex" }}>Movies</h3>
         {MockData.map((item) => (
           <div
             key={item.id}
@@ -34,25 +34,29 @@ function App() {
               <h5>Category: {item.categories}</h5>
               <h5>Year: {item.year}</h5>
               <h5>Director: {item.director}</h5>
-              <img
-                onClick={() => window.open(item.url, "_blank")}
-                src={unFavIcon}
-                alt="unFav icon"
-                style={{
-                  position: "absolute",
-                  top: 5,
-                  right: 20,
-                  fontSize: 20,
-                  cursor: "pointer",
-                }}
-              />
             </div>
+            <img
+              onClick={() => window.open(item.url, "_blank")}
+              src={unFavIcon}
+              alt="unFav icon"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
           </div>
         ))}
       </div>
 
       <div className="favorites-section" style={{ flex: 1 }}>
-        <h3 style={{ marginLeft: 30, margin: "0 auto" }}>Favorite Movies</h3>
+        <h3
+          style={{
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          Favorite Movies
+        </h3>
         {MockData.map((item) => (
           <div
             key={item.id}
@@ -61,7 +65,6 @@ function App() {
               flexWrap: "nowrap",
               display: "flex",
               margin: 20,
-              position: "relative",
             }}
           >
             <img
@@ -74,25 +77,21 @@ function App() {
                 marginTop: 10,
               }}
             />
-
             <div style={{ flex: 1 }}>
               <h5>Name: {item.title}</h5>
               <h5>Category: {item.categories}</h5>
               <h5>Year: {item.year}</h5>
               <h5>Director: {item.director}</h5>
-              <img
-                onClick={() => window.open(item.url, "_blank")}
-                src={favIcon}
-                alt="fav icon"
-                style={{
-                  position: "absolute",
-                  top: 5,
-                  right: 20,
-                  fontSize: 20,
-                  cursor: "pointer",
-                }}
-              />
             </div>
+            <img
+              onClick={() => window.open(item.url, "_blank")}
+              src={favIcon}
+              alt="fav icon"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
           </div>
         ))}
       </div>
